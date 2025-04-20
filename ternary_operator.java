3f05294a759a97a90c5429885e2e7065b6e97187
Anasur_Hossain <anasurhossain@users.noreply.github.com>
2025-04-20 11:05:29 +1000

    ternary operator practice

---
+++ ternary_operator.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        //ternary operator
+
+        int mark= 55;
+        String result = (mark >= 50) ? "Pass": "Fail";
+        System.out.println(result);
+    }
+}
