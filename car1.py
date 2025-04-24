5c1a3e859123372b8f700e258c66d429203e4bfe
Anasur_Hossain <anasurhossain@users.noreply.github.com>
2025-04-24 16:56:56 +1000

    again

--- car1.py
+++ car1.py
@@ -1,11 +1,2 @@
-from car import Car
-
-car1 = Car("Mustang", 2020, "Red", False)
-car2 = Car("AMG", 2024, "White", True)
-car3 = Car("Charger", 2025, "Blue", False)
-
-car2.drive()
-
-car1.stop()
-car3.describe()
+import this
         
