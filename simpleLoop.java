3cbb068f61869b1f41be23f36b59f90a3cac131b
Anasur_Hossain <anasurhossain@users.noreply.github.com>
2025-05-10 10:57:03 +1000

    Simple for loop

---
+++ simpleLoop.java
@@ -0,0 +1,11 @@
+public class simpleLoop {
+    
+    public static void main(String[] args) {
+        // This is a simple for loop that prints numbers from 1 to 5.
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("The current number is: " + i);
+        }
+
+        System.out.println("\nLoop finished!");
+    }
+}
