6551e708d5a592e5f990959996134312d55c3699
Anasur_Hossain <anasurhossain@users.noreply.github.com>
2025-05-11 10:38:05 +1000

    Static class

---
+++ friendOob.java
@@ -0,0 +1,8 @@
+public class friendOob {
+    public static void main(String[] args) {
+        Friend friend1 = new Friend("Spongbob");
+        Friend friend2 = new Friend("Patric");
+
+        Friend.showFriends();
+    }
+}
