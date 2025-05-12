7a11feb51b65a60c785e0df604e4400f6ffce818
Anasur_Hossain <anasurhossain@users.noreply.github.com>
2025-05-12 21:14:38 +1000

    practice

---
+++ beginner23.java
@@ -0,0 +1,14 @@
+public class beginner23 {
+    public static void main(String[] args) {
+        // Print a welcome message
+        System.out.println("Hello, Java beginner!");
+
+        // Simple math
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+
+        // Print the result
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
