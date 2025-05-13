486749e064b8dde76f24f9b1610eec6b94d84c18
Anasur_Hossain <anasurhossain@users.noreply.github.com>
2025-05-13 20:35:08 +1000

    Overriding Method

---
+++ overriding.java
@@ -0,0 +1,11 @@
+public class overriding{
+    public static void main(String[] args) {
+        //method overriding
+
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+
+        dog.move();
+        fish.move();
+    }
+}
