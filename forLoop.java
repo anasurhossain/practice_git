5280b48049375a177f14be05ca2176929bc8da2b
Anasur_Hossain <anasurhossain@users.noreply.github.com>
2025-05-30 21:24:47 +1000

    loop

---
+++ forLoop.java
@@ -0,0 +1,14 @@
+public class forLoop{
+    public static void main(String[] args) {
+        // This for loop will count from 1 to 5
+
+        // Initialization: int i = 1; (starts 'i' at 1)
+        // Condition: i <= 5; (continues as long as 'i' is less than or equal to 5)
+        // Increment: i++; (adds 1 to 'i' after each loop iteration)
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Current count: " + i);
+        }
+
+        System.out.println("\nLoop finished!");
+    }
+}
