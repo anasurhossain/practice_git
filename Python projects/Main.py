6eecc6befd0bfca59c456f5ff99e56f41cc118cc
Anasur_Hossain <anasurhossain@users.noreply.github.com>
2025-07-05 09:07:11 +1000

    initial push

---
+++ Python projects/Main.py
@@ -0,0 +1,3 @@
+print("Hello World!")
+print("I like pizza")
+#this is the code comment
---
+++ Python projects/String.py
@@ -0,0 +1,16 @@
+# String
+
+first_name = "Adam"
+age = 25
+price = 10.99
+is_Student = True
+
+print(f"Hello {first_name}")
+print(f"You are {age} years old")
+print(f"The price is $ {price}")
+print((f"Are you a student? :{is_Student}"))
+
+if is_Student:
+    print("You are a student")
+else:
+    print("You're not a student")
