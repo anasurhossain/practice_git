1a543a88cf45c387faf718b681e1e6ef54729de5
Anasur_Hossain <anasurhossain@users.noreply.github.com>
2025-08-17 21:12:38 +1000

    Init

---
+++ Python projects/Madlib.py
@@ -0,0 +1,11 @@
+#Madlibs Game
+
+Noun1 = input("Enter a Noun: ")
+Adjective1 = input("Enter an adjective: ")
+Verb1 = input("Enter a verb: ")
+
+
+print(f"\nToday I went to {Noun1} for shopping")
+print(f"Then I was feeling {Adjective1}, because missing of the  rear number plate.\n ")
+print(f"I started {Verb1}.")
+
