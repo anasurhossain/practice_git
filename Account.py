a91f36b1589465a1954bca95dc4be6dce2f36091
Anasur_Hossain <anasurhossain@users.noreply.github.com>
2025-08-21 20:45:25 +1000

    p

---
+++ Account.py
@@ -0,0 +1,11 @@
+Deposite = int(input("Please, enter your deposite amount: "))
+
+own_money = True
+
+
+if Deposite >= 500:
+    print("Take a moment it will be added to your account.")
+
+
+else:
+    print("Invalid transaction")
