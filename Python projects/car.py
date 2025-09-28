1817e0bfe56c0ba6908f0093fb71057982c342ec
Anasur_Hossain <anasurhossain@users.noreply.github.com>
2025-09-28 11:13:35 +1000

    Object Oriented Programming

---
+++ Python projects/car.py
@@ -0,0 +1,10 @@
+from OOB import Super_Car
+
+car1 = Super_Car("Pagani", 2024, "white", True)
+car2 = Super_Car("GTR", 2015, "Blue", False)
+car3 = Super_Car("Supra", 2020, "Yellow", True)
+
+print(car3.color)
+car1.drive()
+car2.stop()
+car3.describe()
